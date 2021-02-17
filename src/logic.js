f65e2bab3b4740298a69a28fc20b926a48240a60
britathompson <74376336+britathompson@users.noreply.github.com>
2021-02-16 18:35:40 -0600

    Added icon from API for current weather

--- src/logic.js
+++ src/logic.js
@@ -48,7 +48,8 @@ function showData(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#current-weather-description").innerHTML = response.data.weather[0].description;
-  console.log(response.data);
+  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
 }
 
 function fetchData(city) {
